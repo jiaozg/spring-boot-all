cb811f31f9fe56631e6e6dde66bdb79e8fbf8302
jiaozg <jiaozhiguang@126.com>
2017-09-19 11:14:38 +0800

    update

--- springboot-jvm/src/main/java/com/example/demo/ClassLoaderTest.java
+++ springboot-jvm/src/main/java/com/example/demo/ClassLoaderTest.java
@@ -47,6 +47,10 @@ import java.math.BigDecimal;
 public class ClassLoaderTest {
     public static void main(String[] args) {
 
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        System.out.println(loader);
+        System.out.println(loader.getParent());
+        System.out.println(loader.getParent().getParent());
 
         BigDecimal bigDecimal = new BigDecimal(10);
         System.out.println(bigDecimal + "test");
